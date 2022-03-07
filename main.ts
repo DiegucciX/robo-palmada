377c040bedccfe762422d2e57d5a1dbf3574a3c7
DiegucciX <98874031+DiegucciX@users.noreply.github.com>
2022-03-07 11:56:44 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,16 @@
-input.onSound(DetectedSound.Loud, function () {
-    MOTOR_oN = !(MOTOR_oN)
-    if (MOTOR_oN) {
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    }
-})
-let MOTOR_oN = false
-input.setSoundThreshold(SoundThreshold.Loud, 160)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 5) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 20) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+        basic.pause(250)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+        basic.pause(600)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 150)
+        basic.pause(200)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
     }
 })
